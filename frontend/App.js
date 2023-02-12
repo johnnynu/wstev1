20e9b625a2527718956a869d7515e1c928075d00
[zwroesch@gmail.com] <zwroesch@gmail.com>
2023-02-11 18:31:04 -0800

    BottomTabBar set up with new links needed

--- frontend/App.js
+++ frontend/App.js
@@ -3,17 +3,61 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Provider as PaperProvider } from "react-native-paper";
 import Home from "./src/screens/Home";
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+// const Stack = createNativeStackNavigator();;
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Stack = createNativeStackNavigator();
+const BottomTab = createMaterialBottomTabNavigator();
 
 const App = () => {
   return (
     <>
       <NavigationContainer>
         <PaperProvider>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-          </Stack.Navigator>
+          <BottomTab.Navigator
+            initialRouteName="Home" component={Home}
+            >
+              <BottomTab.Screen
+                name="Search"
+                component= {Home} // Change to search page
+                options={{
+                  tabBarLabel: 'Search',
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name = "home" color={color} size={26} /> // Change Icon to search icon
+                  ),
+                }}
+              />
+              <BottomTab.Screen
+                name="Scan"
+                component= {Home} // Change to Scan page
+                options={{
+                  tabBarLabel: 'Scan',
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name = "home" color={color} size={26} /> // Change Icon to Scan icon
+                  ),
+                }}
+              />
+              <BottomTab.Screen
+                name="Extras"
+                component= {Home} // Change to Extras page
+                options={{
+                  tabBarLabel: 'Extras',
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name = "home" color={color} size={26} /> // Change Icon to extras icon
+                  ),
+                }}
+              />
+              <BottomTab.Screen
+                name="Account"
+                component= {Home} // Change to Account page
+                options={{
+                  tabBarLabel: 'Account',
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons name = "home" color={color} size={26} /> // Change Icon to Account icon
+                  ),
+                }}
+              />
+          </BottomTab.Navigator>
         </PaperProvider>
       </NavigationContainer>
     </>
